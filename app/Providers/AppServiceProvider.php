a0f70367b8892dccec12296c8cc159beea3c1889
Hope <hopewanroy@gmail.com>
2017-10-02 13:55:42 -0700

    account for string length error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+      Schema::defaultStringLength(191);
         //
     }
 
@@ -26,3 +28,10 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 }
+
+// use Illuminate\Support\Facades\Schema;
+
+// public function boot()
+// {
+//     Schema::defaultStringLength(191);
+// }
