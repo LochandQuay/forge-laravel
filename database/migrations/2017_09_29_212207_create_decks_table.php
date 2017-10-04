3df697db518e2a43a2bb46c23dded6113f10b737
Hope <hopewanroy@gmail.com>
2017-10-03 17:22:00 -0700

    update decks to have owners

--- database/migrations/2017_09_29_212207_create_decks_table.php
+++ database/migrations/2017_09_29_212207_create_decks_table.php
@@ -16,13 +16,16 @@ class CreateDecksTable extends Migration
         Schema::create('decks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('type')->default('Standard');
             $table->integer('class_id')->unsigned();
             $table->timestamps();
 
-            $table->unique(['name', 'type', 'class_id']);
+            $table->unique(['name', 'type', 'class_id', 'user_id']);
             $table->index('type');
             $table->index('class_id');
+            $table->index('user_id');
         });
     }
 
